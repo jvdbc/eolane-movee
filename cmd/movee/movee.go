aa88db9e3caae1b3d4180e7494c66ff442fc7797
jvdbc <julien.vdbc@outlook.com>
2018-02-18 13:12:08 +0100

    eolane-movee first commit

---
+++ cmd/movee/movee.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s: frame1frame2frameN\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		flag.Usage()
+	}
+
+}
